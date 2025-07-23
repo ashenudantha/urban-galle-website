cb41d699e18b10be5f16952cd38cfa974c6a813e
ashenudantha <ashenudantha72@gmail.com>
2025-07-23 14:36:00 +0530

    updated loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Amenities from './components/Amenities';
-import Gallery from './components/Gallery';
-import Location from './components/Location';
-import Reviews from './components/Reviews';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import { React, useEffect , useState } from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Amenities from "./components/Amenities";
+import Gallery from "./components/Gallery";
+import Location from "./components/Location";
+import Reviews from "./components/Reviews";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Loader from "./components/loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false); // Remove loader after 2 seconds
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+  
   return (
     <div className="min-h-screen bg-white">
       <Header />
-      <Hero />
-      <About />
+      {loading ? <Loader /> : <Hero />} <About />
       <Amenities />
       <Gallery />
       <Location />
