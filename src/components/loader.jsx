e784b426d33c9020d553a03161e3e87cd62ab4e2
ashenudantha <ashenudantha72@gmail.com>
2025-07-23 15:03:34 +0530

    update loader jsx

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -1,11 +1,19 @@
-import { React} from "react";
+import React from "react";
 import "../style/Loader.css";
-import image from "../images/icons8-dot-50.png" 
+import background from "../images/loader.jpg";
+import spinner from "../images/icons8-dot-50.png";
 
 const Loader = () => {
   return (
-    <div className="loader-wrapper">
-      <img src={image} alt="Logo" className="logo-loader" />
+    <div
+      className="loader-wrapper"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <img src={spinner} alt="Loading..." className="logo-loader" />
     </div>
   );
 };
