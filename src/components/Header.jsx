82353908a43d0095147ddbdaf0ec22bb908aeee5
ashenudantha <ashenudantha72@gmail.com>
2025-07-26 17:28:16 +0530

    added border to logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,24 +32,25 @@ const Header = () => {
       <div className="container mx-auto px-4 py-4">
         <div className="flex justify-between items-center">
           {/* Logo */}
-                <div className="flex items-center space-x-3">
-          <div className={`w-10 h-10 rounded-xl overflow-hidden transition-all duration-300 ${
-            isScrolled ? 'shadow-lg' : ''
-          }`}>
-            <img
-              src="src/images/urban villa logo.jpg"
-              alt="Urban Galle Logo"
-              className="w-full h-full object-cover"
-            />
-          </div>
-          <a href="#home">
-            <h1 className={`text-xl font-bold transition-colors duration-300 ${
-              isScrolled ? 'text-slate-800' : 'text-white'
+              <div className="flex items-center space-x-3">
+            <div className={`w-10 h-10 rounded-xl overflow-hidden border-2 border-gray-400 shadow-sm transition-all duration-300 ${
+              isScrolled ? 'shadow-lg' : ''
             }`}>
-              Urban Galle Villa
-            </h1>
-          </a>
-        </div>
+              <img
+                src="src/images/urban villa logo.jpg"
+                alt="Urban Galle Logo"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <a href="#home">
+              <h1 className={`text-xl font-bold transition-colors duration-300 ${
+                isScrolled ? 'text-slate-800' : 'text-white'
+              }`}>
+                Urban Galle Villa
+              </h1>
+            </a>
+          </div>
+
 
           
           <nav className="hidden lg:flex items-center space-x-8">
